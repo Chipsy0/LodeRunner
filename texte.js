c25ef9b27fb6d0fc225ab6e558ea940c6e03d897
Rmoney3663 <razvan.pruteanu2004@yahoo.ca>
2023-03-05 16:35:47 -0500

    fixed falling sound

--- texte.js
+++ texte.js
@@ -24,13 +24,6 @@ function dessinerDonnees() {
     objC2D.restore();
 }
 
-// Pour incrire les données
-function dessinerGameover() {
-    objC2D.save();
-    
-    objC2D.restore();
-}
-
 // Pour debuter le chrono
 function debutChrono() {
     objDateDebut = new Date();
