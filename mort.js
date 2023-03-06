642ba3a3dde69a87460cd608c9bfde6a29ab2059
Rmoney3663 <razvan.pruteanu2004@yahoo.ca>
2023-03-05 21:50:58 -0500

    fixed so death sound only once

--- mort.js
+++ mort.js
@@ -1,24 +1,24 @@
 //si Lode runner meurt 
 function mort() {
-        objRunner.intY -= 1 * objRunner.intVitesse;
-        if (Math.floor(objRunner.intY / 30) == 0 ) {
-            intNbVies--;
-            binMort = false;
-            binCommence = false;
-            if (intNbVies != 0){
-                debutChrono();
-                intScore -= intScoreNiveau;
-                intScoreNiveau = 0
-                initRunner();
-                initTableau();
-                intnbLingot = 0;
-            }
+    objRunner.intY -= 1 * objRunner.intVitesse;
+    if (Math.floor(objRunner.intY / 30) == 0) {
+        intNbVies--;
+        binMort = false;
+        binCommence = false;
+        if (intNbVies != 0) {
+            debutChrono();
+            intScore -= intScoreNiveau;
+            intScoreNiveau = 0
+            initRunner();
+            initTableau();
+            intnbLingot = 0;
         }
+    }
 }
 
 //si Lode runner meurt grace à une garde
-function mortParGarde(){
-    for (var i = 0; i < intNbGardes; i++){
+function mortParGarde() {
+    for (var i = 0; i < intNbGardes; i++) {
         var objGarde = tabGardes[i];
         intPosX = objGarde.intX;
         intPosY = objGarde.intY;
@@ -26,17 +26,20 @@ function mortParGarde(){
         intHauteur = objGarde.intHauteur;
 
         if ((((Math.floor(objRunner.intY) >= intPosY && Math.floor(objRunner.intY) <= (intPosY + intHauteur))
-           && (Math.floor(objRunner.intX) >= intPosX && Math.floor(objRunner.intX) <= (intPosX + intLargeur)))
-           || (((Math.floor(objRunner.intY)) >= intPosY && (Math.floor(objRunner.intY)) <= (intPosY + intHauteur))
-           && ((Math.floor(objRunner.intX) + 18) >= intPosX && (Math.floor(objRunner.intX) + 18) <= (intPosX + intLargeur))))   
+            && (Math.floor(objRunner.intX) >= intPosX && Math.floor(objRunner.intX) <= (intPosX + intLargeur)))
+            || (((Math.floor(objRunner.intY)) >= intPosY && (Math.floor(objRunner.intY)) <= (intPosY + intHauteur))
+                && ((Math.floor(objRunner.intX) + 18) >= intPosX && (Math.floor(objRunner.intX) + 18) <= (intPosX + intLargeur))))
 
-           || ((((Math.floor(objRunner.intY) + 28)>= intPosY &&(Math.floor(objRunner.intY) + 28) <= (intPosY + intHauteur))
-           && (Math.floor(objRunner.intX)         >= intPosX && Math.floor(objRunner.intX)       <= (intPosX + intLargeur)))
-           || (((Math.floor(objRunner.intY) + 28) >= intPosY && (Math.floor(objRunner.intY) + 28)<= (intPosY + intHauteur))
-           && ((Math.floor(objRunner.intX) + 18)  >= intPosX && (Math.floor(objRunner.intX) + 18)<= (intPosX + intLargeur))))){  
-            objSons.dead.play();       
-            binMort = true;
-            
+            || ((((Math.floor(objRunner.intY) + 28) >= intPosY && (Math.floor(objRunner.intY) + 28) <= (intPosY + intHauteur))
+                && (Math.floor(objRunner.intX) >= intPosX && Math.floor(objRunner.intX) <= (intPosX + intLargeur)))
+                || (((Math.floor(objRunner.intY) + 28) >= intPosY && (Math.floor(objRunner.intY) + 28) <= (intPosY + intHauteur))
+                    && ((Math.floor(objRunner.intX) + 18) >= intPosX && (Math.floor(objRunner.intX) + 18) <= (intPosX + intLargeur))))) {
+            if (binMort == false) {
+                objSons.dead.play();
+                binMort = true;
+            }
+
+
         }
     }
 }
